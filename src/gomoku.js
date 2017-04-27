2ab52e2e692e9ed839ee266fd4b9f73cb5fb1855
wl00887404 <wl00887404@gmail.com>
2017-04-27 21:16:12 +0800

    Update gomoku.js

--- src/gomoku.js
+++ src/gomoku.js
@@ -68,7 +68,7 @@ export default class Gomoku {
             return
         }
 
-        this.log(`${this.state.nowPlayer} undo`)
+        this.log(`${this.state.nowPlayer.name} undo`)
         this.state.turn -= 1
         this.state.nowPlayer = this.players[this.state.turn % this.players.length]
         this.state.nowPlayer.undoMove()
